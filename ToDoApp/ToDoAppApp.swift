ede4b757c9e0bccb30fcf0ce686c093c33286144
divya <divyaaddepalle@gmail.com>
2023-07-18 10:59:58 -0500

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
